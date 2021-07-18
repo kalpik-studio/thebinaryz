c0fcf3cd18ae76cd4a5908ade8d45a5507ca0a47
Devansh <devansh.madd@gmail.com>
2021-07-18 19:12:12 +0400

    information

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          The Binaryz aims to provides services in segments of IT Security, Robotics and IoT across government, healthcare, education, BFSI and enterprise verticals.
           </p>
         </div>
         <div className='row'>
