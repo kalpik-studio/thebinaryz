d28188ed57e81ff4209dded666aba6aaace71657
Devansh <devansh.madd@gmail.com>
2021-08-14 19:06:26 +0400

    Carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Navigation } from './components/navigation'
-import { Header } from './components/header'
+// import { Header } from './components/header'
 import { Gallery } from './components/gallery'
 import { About } from './components/about'
 import { Services } from './components/services'
@@ -10,6 +10,12 @@ import JsonData from './data/data.json'
 import SmoothScroll from 'smooth-scroll'
 import ScrollButton from './components/ScrollButton/ScrollButton';
 import React from 'react';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+// import { MainCarousel } from './components/MainCarousel'
+import './App.css';
+
+
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -25,7 +31,10 @@ const App = () => {
   return (
     <div>
       <Navigation />
-      <Header data={landingPageData.Header} />
+      {/* <Header data={landingPageData.Header} /> */}
+      {/* <Headerr/> */}
+      <ImageSlider slides={SliderData} />
+      {/* <MainCarousel/> */}
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Testimonials data={landingPageData.Testimonials} />
---
+++ src/components/SliderData.js
@@ -0,0 +1,24 @@
+import Robotics from './images/Main1.jpg';
+import Drone from './images/Main2.jpg';
+import CyberSec from './images/Main4.jpg';
+import IOT from './images/Main3.jpg';
+import Main from './images/Main6.jpg';
+
+export const SliderData = [
+  {
+    image: Main
+  },
+  {
+    image: Robotics
+  },
+  {
+    image: Drone
+  },
+  {
+    image: CyberSec
+  },
+  {
+    image:IOT
+  }
+
+];
